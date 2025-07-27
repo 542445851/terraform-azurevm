1e66cdc30e71bf42569619d3c42120dca4deff03
Deepak_Kadam <33250810+542445851@users.noreply.github.com>
2025-07-27 20:30:14 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "devstorage"
+    storage_account_name = "demotfstatedevstorage"
     container_name = "devcontainer"
     key = "dev.terrafrom.tfstate"
     
